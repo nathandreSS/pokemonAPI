24785e73057974a8dc8908a6542290b3f051ef88
Nathan André <nathan_99_@hotmail.com>
2021-04-11 21:58:01 -0300

    ainda ha esperança

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,9 @@ module.exports = {
   "url": process.env.DATABASE_URL,
   "entities": [process.env.ENTITIES],
   "migrations": [process.env.MIGRATIONS],
-  "ssl": true,
+  "ssl": {
+    "rejectUnauthorized": false,
+  },
   "cli": {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
